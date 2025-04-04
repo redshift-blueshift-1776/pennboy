e78c07fa06433d91ef6d45efff94acc534c45405
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-04-03 20:18:24 -0400

    Add cursor setter gameobject and script

---
+++ Assets/PennBoy/Common/Cursor/CursorSetter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorSetter : MonoBehaviour
+{
+    [SerializeField] private bool cursorVisibility;
+    [SerializeField] private CursorLockMode cursorLockMode;
+
+    void Awake() {
+        // set cursor visibility and lockstate
+        Cursor.visible = cursorVisibility;
+        Cursor.lockState = cursorLockMode;
+        // destroy the object
+        Destroy(this);
+    }
+}
