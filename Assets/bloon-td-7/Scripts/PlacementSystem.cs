347a5aee7f33cb71d24d2237c5644711145bf157
justclaner <justclaner@gmail.com>
2025-04-05 18:44:35 -0400

    Revert "added ui for current mode"
    This reverts commit 90e59e658a2d13fb4088b1975bfca2fd6f28f2cf.

--- Assets/bloon-td-7/Scripts/PlacementSystem.cs
+++ Assets/bloon-td-7/Scripts/PlacementSystem.cs
@@ -14,14 +14,12 @@ public class PlacementSystem : MonoBehaviour
     private PlacementMode currentMode;
     private List<Tower> towersToSacrifice;
     private int totalCost;
-    [SerializeField] private TMP_Text modeText;
 
     private void Start()
     {
         totalCost = 0;
         mouseIndicator.SetActive(false);
         currentMode = PlacementMode.Selection;
-        modeText.text = "Current mode: Placement Mode";
     }
 
     private void Update()
@@ -30,15 +28,9 @@ public class PlacementSystem : MonoBehaviour
         if (currentMode != PlacementMode.Selection && cardUsing.canSacrifice && Input.GetKeyDown(KeyCode.Q))
         {
             if (currentMode == PlacementMode.PlacingTower)
-            {
                 currentMode = PlacementMode.Sacrificing;
-                modeText.text = "Current mode: Sacrifice Mode";
-            }
             else
-            {
                 currentMode = PlacementMode.PlacingTower;
-                modeText.text = "Current mode: Placement Mode";
-            }
         }
 
         switch (currentMode)
