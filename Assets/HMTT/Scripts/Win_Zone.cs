4ddc95c0d493e8ae3ee9e0b218256cd3994c966d
Forest Ho-Chen <foresth168@gmail.com>
2025-04-07 16:37:26 -0400

    Parkour Endscreen

---
+++ Assets/HMTT/Scripts/Win_Zone.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win_Zone : MonoBehaviour
+{
+    //[SerializeField]
+    //Game level;
+
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    GameObject winText;
+
+    [SerializeField]
+    GameObject restartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.SetActive(false);
+        restartButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if(col.gameObject == player) {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            winText.SetActive(true);
+            restartButton.SetActive(true);
+        }
+    }
+
+    public void RestartLevel() {
+        SceneManager.LoadScene("Level1Test");
+    }
+}
