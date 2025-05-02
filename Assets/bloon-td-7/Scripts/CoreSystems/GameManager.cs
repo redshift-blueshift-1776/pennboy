7a2e2c621973d7bbe65ba06c9606ebba6e3a702b
Forest Ho-Chen <foresth168@gmail.com>
2025-05-02 11:50:47 -0400

    Mac Build

--- Assets/bloon-td-7/Scripts/CoreSystems/GameManager.cs
+++ Assets/bloon-td-7/Scripts/CoreSystems/GameManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using UnityEditor.PackageManager;
+//using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
