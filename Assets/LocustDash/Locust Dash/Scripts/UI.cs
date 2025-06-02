51176ca26fe70c8a0a97b1a66698b4792597f8a4
Forest Ho-Chen <foresth168@gmail.com>
2025-06-01 21:43:55 -0400

    New Mac Version?

--- Assets/LocustDash/Locust Dash/Scripts/UI.cs
+++ Assets/LocustDash/Locust Dash/Scripts/UI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.PackageManager;
+//using UnityEditor.PackageManager;
 public class UI : MonoBehaviour
 {
      public TMP_Text scoreText;
